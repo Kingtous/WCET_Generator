2b1354eb2621be7be594c6b57e4cbc5eb96b4456
liuuujr <1075876650@qq.com>
2019-08-16 12:57:11 +0800

    graph 指定文件生成pdf图
    指令：python3 graph.py -d dot文件路径 -r relation文件路径 -w wct文件路径
    
    生成的所有文件都在dot文件目录下
    
    三个参数的格式可以为
    绝对路径：必须以/开头
    相对路径：格式1：文件夹/文件   格式2： ./文件夹/文件   格式3： ../文件夹/文件

--- src/Preprocessing/graph.py
+++ src/Preprocessing/graph.py
@@ -1,26 +1,31 @@
 import networkx as nx
+import sys,getopt
 from networkx.drawing.nx_pydot import write_dot
 import re
-
+import os
 #=========================
 from PreprocessDot import preprocess
 #=========================
 
 #设置工作目录
-root='/home/rtco/Desktop/Bots_OpenMP_Tasks/sort/PCFG/'
+# root='/home/rtco/Desktop/Bots_OpenMP_Tasks/sort/PCFG/'
+root='/Users/liuuujr/neww/'
 #=======DOT存放位置===============
-dotPath=root+'sort_sweet.dot'
+dotPath=root+'sort_sw.dot'
 #=======relation.txt存放位置======
-relationPath=root+'relation.txt'
+relationPath=root+'relati.txt'
 #=======需要处理的函数入口（暂时不用）======
 parseFunction='_thrFunc0_'
 #=======WCET目录====================
-wctPath=root+'sort.wct'
+wctPath=root+'sot.wct'
 #===========cluster_定义==========
 Definition=''
 #========输出================================
 #=======dot输出==========
 dotOutput=root+'sort_pro.dot'
+
+
+
 #=======特征输出==========
 Edges=0
 Nodes=0
@@ -404,29 +409,122 @@ def calcBranch(graph):
             # >1 表示有条件分支
             TotalConditionBranch=TotalConditionBranch+max(numset)
 
+#处理输入的文件路径
+def init_argv(Argslist,current_dir):
+    global dotPath,relationPath,wctPath,root,dotOutput
+
+    # # =======DOT存放位置===============
+    if os.path.isabs(Argslist['d']) == False:# 相对路径
+        if Argslist['d'].find("../") != -1:                #相对路径格式为"格式1：../文件夹/文件"
+            dotPath = current_dir[:current_dir[:-1].rfind("/")+1] + Argslist['d'][3:]
+        elif Argslist['d'].find("./") != -1:
+            wctPath = current_dir + Argslist['d'][2:]
+        else:
+            dotPath = current_dir + Argslist['d']    #相对路径格式为"格式1：文件夹/文件   格式2： ./文件夹/文件"
+
+    else:                                    # 绝对路径
+        dotPath = Argslist['d']
+    # # =======relation存放位置===============
+    if os.path.isabs(Argslist['r']) == False:
+        if Argslist['r'].find("../") != -1:
+            relationPath = current_dir[:current_dir[:-1].rfind("/")+1] + Argslist['r'][3:]
+        elif Argslist['r'].find("./") != -1:
+            wctPath = current_dir + Argslist['r'][2:]
+        else:
+            relationPath = current_dir + Argslist['r']
+
+    else:
+        relationPath = Argslist['r']
+
+    # # =======wct存放位置===============
+    if os.path.isabs(Argslist['w']) == False:
+        if Argslist['w'].find("../") != -1:
+            wctPath = current_dir[:current_dir[:-1].rfind("/")+1] + Argslist['w'][3:]
+
+        elif Argslist['w'].find("./") != -1:
+            wctPath = current_dir + Argslist['w'][2:]
+        else:
+            wctPath = current_dir + Argslist['w']
+
+    else:
+        wctPath = Argslist['w']
+
+    # # =======root目录===============好像没用了
+    root_flag_loc = dotPath.rfind("/")
+    root = dotPath[:root_flag_loc+1]
+    # # =======dotOutput===============
+    dotOutput = dotPath[: dotPath.rfind(".")] + "_pro.dot"
+
+
+
 if __name__=='__main__':
-    print("处理Relation表...")
-    relation = parseRelation(relationPath)
-    # 预处理
-    print("预处理CFG...")
-    preprocess(dotPath)
-    # 得到cluster定义
-    print("获取图的Cluster__定义...")
-    Definition=open(dotPath+'_dec').read()
-    # 调用networkx处理CFG
-    print("处理CFG图...")
-    graph = nx.nx_pydot.read_dot(dotPath+'_pd')
-    parse(parseFunction,graph,relation)
-    write_dot(graph,dotOutput)
-    # 加入定义
-    print("加入图的Cluster__定义...")
-    fileContext=open(dotOutput,'r').readlines()
-    FinalOutput=open(dotOutput+'_Final','w')
-    for line in fileContext[:-1]:
-        FinalOutput.write(line)
-    print("生成最终Dot图...")
-    FinalOutput.write(Definition)
-    FinalOutput.close()
-    print("生成最终PDF...")
-    # 调用系统 graphviz生成最终的dot
-    pdfPrint(dotOutput+'_Final')
+    #currentdir = os.path.dirname(__file__) + "/"
+    currentdir = os.getcwd() + "/"
+    op_p = False #path
+    op_d = False #dot
+    op_r = False #relation
+    op_w = False #wct
+    # Process
+    getArgs = {}
+    options, args = getopt.getopt(sys.argv[1:], "d:r:w:")
+    for op, value in options:
+
+        if op == '-d':
+            op_d = True
+            # # =======DOT存放位置===============
+           # dotPath = current_dir + value
+            getArgs['d'] = value
+            continue
+        if op == '-r':
+            op_r = True
+            getArgs['r'] = value
+            # # =======需要处理的函数入口（暂时不用）======
+            #relationPath =current_dir + value
+            continue
+        if op == '-w':
+            op_w = True
+            getArgs['w'] = value
+            # # =======WCET目录====================
+            #wctPath =current_dir + value
+
+
+
+    if (op_d == False)or(op_r == False)or(op_w == False):
+        print('Please Import the file u\'d like to ')
+
+        # //print(current_dir1)
+    else:
+
+        init_argv(getArgs,currentdir)
+
+
+        print("处理Relation表...")
+        relation = parseRelation(relationPath)
+        # 预处理
+        print("预处理CFG...")
+        preprocess(dotPath)
+        # 得到cluster定义
+        print("获取图的Cluster__定义...")
+        Definition=open(dotPath+'_dec').read()
+        # 调用networkx处理CFG
+        print("处理CFG图...")
+        graph = nx.nx_pydot.read_dot(dotPath+'_pd')
+        parse(parseFunction,graph,relation)
+        write_dot(graph,dotOutput)
+        # 加入定义
+        print("加入图的Cluster__定义...")
+        fileContext=open(dotOutput,'r').readlines()
+        FinalOutput=open(dotOutput+'_Final','w')
+        for line in fileContext[:-1]:
+            FinalOutput.write(line)
+        print("生成最终Dot图...")
+        FinalOutput.write(Definition)
+        FinalOutput.close()
+        print("生成最终PDF...")
+        # 调用系统 graphviz生成最终的dot
+        pdfPrint(dotOutput+'_Final')
+
+else:
+    print("Please run this scripts directly.")
+
+
